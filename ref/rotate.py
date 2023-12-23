62831d458c9b88ec1dd9ff723aed03bc1efb959c
lake041 <lake041@korea.ac.kr>
2023-12-23 20:14:17 +0900

    231223: 토요일

--- ref/rotate.py
+++ ref/rotate.py
@@ -10,16 +10,21 @@ for row in list(map(list, zip(*(arr)[::-1]))):
     print(row)
 print()
 
-print('2.반시계방향')
+print('2. 반시계방향')
 for row in list(map(list, zip(*(arr))))[::-1]:
     print(row)
 print()
 
-print('3. y = -x 대칭이동')
+print('3. 180도 회전')
+for row in [row[::-1] for row in arr][::-1]:
+    print(row)
+print()
+
+print('4. y = -x 대칭이동')
 for row in list(map(list, zip(*(arr)))):
     print(row)
 print()
 
-print('4. y = x 대칭이동')
+print('5. y = x 대칭이동')
 for row in list(map(list, zip(*(arr[::-1]))))[::-1]:
     print(row)
