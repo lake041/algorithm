b5b5d6642c9a6f9a1230b2948b36a22c4003fac7
lake041 <lake041@korea.ac.kr>
2023-12-29 10:28:28 +0900

    DOCS: README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# algorithm
+## 코딩테스트를 위한 알고리즘 풀이 저장소
+
+### 언어
+
+-   자바스크립트
+-   파이썬
+
+### 문제
+
+-   [프로그래머스](https://school.programmers.co.kr/learn/challenges?order=recent&page=1&languages=python3%2Cjavascript&levels=2%2C3)
+-   [백준 CLASS](https://solved.ac/class)
+-   [리트코드 75](https://leetcode.com/studyplan/leetcode-75/)
--- 라이브러리/파이썬/dijkstra.py
+++ 라이브러리/파이썬/dijkstra.py
@@ -1,41 +1,20 @@
-from collections import defaultdict
-from heapq import heappush, heappop
-from sys import stdin, maxsize
-input = stdin.readline
+from sys import maxsize
+from heapq import heappop, heappush
 
-N, M, D, E = map(int, input().split())
-hs = [0] + list(map(int, input().split()))
-routes = defaultdict(dict)
-for _ in range(M):
-    a, b, n = map(int, input().split())
-    routes[a].setdefault(b, n)
-    routes[b].setdefault(a, n)
-    # routes[a].update(b=n)
-    # routes[b].update(a=n)
-print(routes)
-
-def dijkstra(graph, start):
-    # distance: start로부터의 거리값
-    distance = {node: maxsize for node in graph}
-    # 시작 값은 자기 자신이므로 거리가 0임
+def dijkstra(start, graph, N):
+    distance = [maxsize]*(N+1)
     distance[start] = 0
-    q = []
-    # 가까운 거리부터 탐색하므로 거리 값이 먼저 옴
-    heappush(q, (distance[start], start))
-    
-    while q:
-        cur_dist, cur_dest = heappop(q)
-        # 기존에 있는 거리보다 현재 거리가 더 길다면 볼 필요도 없음
-        if distance[cur_dest] < cur_dist:
-            continue
-        for new_dest, new_dist in graph[cur_dest].items():
-            # 해당 노드를 거쳐 new_dest까지 가는 거리
-            dist = cur_dist + new_dist
-            # 알고 있는 거리보다 작으면 갱신
-            if dist < distance[new_dest]:
-                distance[new_dest] = dist
-                # 다음 인접 거리를 계산 하기 위해 큐에 삽입
-                heappush(q, (dist, new_dest))
-    return distance
 
-print(dijkstra(routes, 1))
+    q = []
+    heappush(q, (distance[start], start))
+    while q:
+        acc_dist, stopover = heappop(q)
+        if distance[stopover] < acc_dist:
+            continue
+        for next_node, plus_dist in graph[stopover]:
+            new_dist = acc_dist + plus_dist
+            if new_dist < distance[next_node]:
+                distance[next_node] = new_dist
+                heappush(q, (distance[next_node], next_node))
+
+    return distance
