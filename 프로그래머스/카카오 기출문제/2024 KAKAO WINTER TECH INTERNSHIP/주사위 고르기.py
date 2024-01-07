225531711c39c907aaba558fb75f2dfa30f5513c
lake041 <lake041@korea.ac.kr>
2024-01-08 02:06:18 +0900

    230108

--- 프로그래머스/카카오 기출문제/2024 KAKAO WINTER TECH INTERNSHIP/주사위 고르기.py
+++ 프로그래머스/카카오 기출문제/2024 KAKAO WINTER TECH INTERNSHIP/주사위 고르기.py
@@ -2,12 +2,11 @@ from itertools import combinations, product
 from bisect import bisect_left
 
 def solution(dices):
-    dp = {}
-
+    dic = {}
     L = len(dices)
     for A_index_combi in combinations(range(L), L//2):
         B_index_combi = [i for i in range(L) if i not in A_index_combi]
-
+    
         A, B = [], []
         for order_product in product(range(6), repeat=L//2):
             A.append(sum(dices[i][j] for i, j in zip(A_index_combi, order_product)))
@@ -15,8 +14,8 @@ def solution(dices):
         B.sort()
 
         wins = sum(bisect_left(B, num) for num in A)
-        dp[wins] = list(A_index_combi)
+        dic[wins] = list(A_index_combi)
 
-    max_key = max(dp.keys())
+    max_key = max(dic.keys())
 
-    return [x+1 for x in dp[max_key]]
+    return [x+1 for x in dic[max_key]]
